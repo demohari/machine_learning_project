f482128abfa1e3d36bc1f6d20f32755ea0488023
demohari <kishore_koneru@yahoo.com>
2023-01-01 13:32:43 +0000

    folder structure created

--- setup.py
+++ setup.py
@@ -1,40 +1,27 @@
-from setuptools import setup
-from typing import List
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-# declaring variables for setup function
+__version__ = "0.0.1"
 
-PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
-AUTHOR = "demohari"
-AUTHOR_EMAIL = "kishore_koneu@yahoo.com"
-PACKAGES = ["housing"]  ## folder name
-DESCRIPTION = "This is a sample ML Project"
-REQUIREMENT_FILE_NAME = "requirements.txt"
-HYPHEN_E_DOT = "-e ."
+REPO_NAME = "machine_learning_project"
+AUTHOR_USER_NAME = "demohari"
+SRC_REPO = "housing"
+AUTHOR_EMAIL = "kishore_koneru@yahoo.com"
 
-
-def get_requirements_list() -> List[str]:
-    """
-    Description: This function is going to return list of requirement
-    mention in requirements.txt file
-    return This function is going to return a list which contain name
-    of libraries mentioned in requirements.txt file
-    """
-    with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-        if HYPHEN_E_DOT in requirement_list:
-            requirement_list.remove(HYPHEN_E_DOT)
-        return requirement_list
-
-
-setup(
-    name=PROJECT_NAME,
-    version=VERSION,
-    author=AUTHOR,
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description=DESCRIPTION,
-    packages=PACKAGES,
-    install_requires=get_requirements_list(),
+    description="A small ml package",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
