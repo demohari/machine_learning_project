c22a116dbcd188d7521bf5cf06ecf313a984f4f8
demohari <kishore_koneru@yahoo.com>
2023-01-05 17:22:24 +0000

    validation done

--- src/housing/entity/artifact_entity.py
+++ src/housing/entity/artifact_entity.py
@@ -2,9 +2,19 @@ from collections import namedtuple
 from unicodedata import name
 
 
-DataIngestionArtifact = namedtuple("DataIngestionArtifact",
-[ "train_file_path", "test_file_path", "is_ingested", "message"])
+DataIngestionArtifact = namedtuple(
+    "DataIngestionArtifact",
+    ["train_file_path", "test_file_path", "is_ingested", "message"],
+)
 
 
-DataValidationArtifact = namedtuple("DataValidationArtifact",
-["schema_file_path","report_file_path","report_page_file_path","is_validated","message"])
+DataValidationArtifact = namedtuple(
+    "DataValidationArtifact",
+    [
+        "schema_file_path",
+        "report_file_path",
+        "report_page_file_path",
+        "is_validated",
+        "message",
+    ],
+)
